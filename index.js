c04806aeeadb7eba010bbb342878a1afe9c0731f
Filip <filip4mk@gmail.com>
2026-03-28 22:09:21 +0100

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from Launchpad! 🚀</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
