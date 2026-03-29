bd48d4df129d7d11f369f390c317390c67ecf482
Filip <filip4mk@gmail.com>
2026-03-29 16:36:41 +0200

    test 3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from Launchpad! UPDATED VERSION 🚀</h1>');
+  res.send('<h1>Hello from Launchpad! UPDATED VERSION V3 🚀</h1>');
 });
 
 app.listen(port, () => {
